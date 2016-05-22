aefa4a8ed9526f256b099aba97cd954a96fb1218
holyshared <holy.shared.design@gmail.com>
2016-05-22 17:18:12 +0900

    Update migration log

--- src/MigrationLogger.hh
+++ src/MigrationLogger.hh
@@ -20,18 +20,18 @@ final class MigrationLogger implements Subscribable<Message>
 
     public function loaded(MigrationLoadedEvent $event): void
     {
-        color\info("Migration");
-        color\info("migration found %d files",  $event->migrationCount());
+        color\info("Database migration started");
+        color\info("Migration found %d files\n",  $event->migrationCount());
     }
 
     public function start(MigrationStartEvent $event): void
     {
-        color\info("query: %s",  $event->query());
+        color\info("query: %s\n",  $event->query());
     }
 
     public function success(MigrationSuccessEvent $event): void
     {
-        color\info("success: %F seconds", $event->totalTime());
+        color\info("success: %F seconds\n", $event->totalTime());
     }
 
 }
