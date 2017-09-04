f685cfe5f1bba7a24a25a39263abf8fee13f92b3
holyshared <holy.shared.design@gmail.com>
2017-09-04 13:21:41 +0900

    add drop command

--- src/Application/Command/DropDatabaseCommand.hh
+++ src/Application/Command/DropDatabaseCommand.hh
@@ -13,19 +13,19 @@ namespace HHPack\Migrate\Application\Command;
 
 use HHPack\Getopt as cli;
 use HHPack\Getopt\Parser\{ OptionParser };
-use HHPack\Migrate\Database\{ Connection, DatabaseClient, DatabaseServer, DatabaseAlreadyExistsException };
-use HHPack\Migrate\Database\Query\{ CreateDatabaseQuery, DatabaseAlreadyExistsQuery };
+use HHPack\Migrate\Database\{ Connection, DatabaseClient, DatabaseServer, DatabaseNotFoundException };
+use HHPack\Migrate\Database\Query\{ DropDatabaseQuery, DatabaseAlreadyExistsQuery };
 use HHPack\Migrate\Application\{ Context, Command };
 use HHPack\Migrate\Application\Configuration\{ Server };
 use RuntimeException;
 
-final class CreateDatabaseCommand extends DatabaseCommand implements Command
+final class DropDatabaseCommand extends DatabaseCommand implements Command
 {
 
     public function __construct()
     {
-        $this->usage = "migrate create [OPTIONS]";
-        $this->description = "Create a database";
+        $this->usage = "migrate drop [OPTIONS]";
+        $this->description = "Drop a database";
         $this->optionParser = cli\optparser([
             cli\on(['-h', '--help'], 'Display help message', () ==> {
                 $this->help = true;
@@ -43,23 +43,23 @@ final class CreateDatabaseCommand extends DatabaseCommand implements Command
             $logger = $context->logger();
             $server = $context->databaseServer();
 
-            $logger->info(sprintf("Create the database %s\n", $server->name()));
-            \HH\Asio\join($this->createDatabase($server));
-            $logger->info(sprintf("%s is created\n", $server->name()));
+            $logger->info(sprintf("Drop the database %s\n", $server->name()));
+            \HH\Asio\join($this->dropDatabase($server));
+            $logger->info(sprintf("%s is droped\n", $server->name()));
         }
     }
 
-    private async function createDatabase(Server $server): Awaitable<void>
+    private async function dropDatabase(Server $server): Awaitable<void>
     {
         $connection = await $this->connectToServer($server);
         $result = await $connection->query(new DatabaseAlreadyExistsQuery($server->name()));
 
-        if (!$result->isEmpty()) {
-            throw new DatabaseAlreadyExistsException(sprintf("Database %s is already exists",
+        if ($result->isEmpty()) {
+            throw new DatabaseNotFoundException(sprintf("Database %s is not found",
                 $server->name()));
         }
 
-        await $connection->query(new CreateDatabaseQuery($server->name()));
+        await $connection->query(new DropDatabaseQuery($server->name()));
     }
 
 }
